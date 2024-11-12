e72d7cc74f23ce7f51b4a13ae5de8d2c46ff3663
Kósa Márk Milán <kosa.mark@verebely.dc>
2024-11-12 14:08:28 +0100

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+document.getElementById('button').onclick = () => {
+    document.getElementById('calculator').style.display = document.getElementById('calculator').style.display == "none" ? "block" : "none"
+}
